3c943e4663f7dc3c6698dfac6f23d09817481ddf
monte.ohrt <monte.ohrt@0ae895b7-b043-0410-bcaf-a191beac5e7e>
2008-06-01 04:21:50 +0000

    add default to database.php (thanks pablo77)

--- trunk/tinymvc/myapp/configs/database.php
+++ trunk/tinymvc/myapp/configs/database.php
@@ -9,15 +9,12 @@
  * @author		Monte Ohrt
  */
 
-
-$config['plugin'] = 'TMVC_PDO'; // plugin for db access
-
-$config['type'] = 'mysql';      // connection type
-$config['host'] = 'localhost';  // db hostname
-$config['name'] = 'dbname';     // db name
-$config['user'] = 'dbuser';     // db username
-$config['pass'] = 'dbpass';     // db password
-
-$config['persistent'] = false;  // db connection persistence?
+$config['default']['plugin'] = 'TMVC_PDO'; // plugin for db access
+$config['default']['type'] = 'mysql';      // connection type
+$config['default']['host'] = 'localhost';  // db hostname
+$config['default']['name'] = 'dbname';     // db name
+$config['default']['user'] = 'dbuser';     // db username
+$config['default']['pass'] = 'dbpass';     // db password
+$config['default']['persistent'] = false;  // db connection persistence?
 
 ?>
